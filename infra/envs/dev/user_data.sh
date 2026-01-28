3ca3d857a2aa94a0a173c8aecf8a7ce46e69e844
Varad Khot <varad.khot@flentas.com>
2026-01-28 13:49:12 +0530

    Update user_data to use emp-task repo

--- infra/envs/dev/user_data.sh
+++ infra/envs/dev/user_data.sh
@@ -5,10 +5,8 @@ yum install -y python3 git
 pip3 install flask psycopg2-binary gunicorn
 
 cd /home/ec2-user
-
-# replace with your real repo
-git clone https://github.com/YOUR_GITHUB_USERNAME/project.git
-cd project/backend
+git clone https://github.com/varadkhot42/emp-task.git
+cd emp-task/backend
 
 export DB_HOST=task-postgres-db.c5iqs864ctd6.ap-south-1.rds.amazonaws.com
 export DB_PORT=5432
