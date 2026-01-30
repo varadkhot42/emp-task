28ea5a00aa38d85217f235b6e1a74a0d797c4a89
Varad Khot <varad.khot@flentas.com>
2026-01-30 11:31:02 +0530

    pw cahnge

--- backend/app/db.py
+++ backend/app/db.py
@@ -6,6 +6,6 @@ def get_db():
         host=os.getenv("DB_HOST", "localhost"),
         database=os.getenv("DB_NAME", "taskdb"),
         user=os.getenv("DB_USER", "postgres"),
-        password=os.getenv("DB_PASS", "postgres"),
+        password=os.getenv("DB_PASS", "varad123"),
         port=os.getenv("DB_PORT", "5432"),
     )
